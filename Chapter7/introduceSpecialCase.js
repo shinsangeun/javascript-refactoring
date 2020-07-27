4b4c7848940c719cde9eeadf60f799cb9155d6a7
shinsangeun <s0813se@naver.com>
2020-07-28 01:36:26 +0900

    리팩터링 7장 업로드 - 코드 예제 업데이트

--- Chapter7/introduceSpecialCase.js
+++ Chapter7/introduceSpecialCase.js
@@ -4,3 +4,12 @@ if(aCustomer === "미확인 고객") customerName = "거주자";
 class UnknownCustomer {
     get name() {return "거주자"}
 }
+
+const aCustomer = site.customer;
+let customerName;
+if(aCustomer === "미확인 고객") customerName = "거주자";
+else customerName = aCustomer.name;
+
+const plan = (aCustomer === "미확인 고객") ? registry.billingPlans.basic : aCustomer.billingPlans;
+
+const weeksDelinquent = (aCustomer === "미확인 고객") ? 0 : aCustomer.paymentHistory.weeksDelinquentInLastYear;
